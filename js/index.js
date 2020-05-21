8af6cef43daa567c9fa799f4eac496afad9b0fae
Aamir Zaidi <aamir.zaidi2000@gmail.com>
2020-05-21 17:03:48 +0530

    Responsive Home Page Done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 // @ts-nocheck
+const mobile_btn = $(".navItem_wrapper > i");
+const menu = $(".navItem_wrapper > ul");
+
+
 
 const redirectContact = () => {
     window.location.href = "./index.html#contactMe";
@@ -7,3 +11,18 @@ const redirectContact = () => {
 const redirectAbout = () => {
     window.location.href = "./about.html";
 }
+
+
+const menuToggle = () => {
+    $(menu).toggleClass("show");
+}
+
+
+$(mobile_btn).on("click", () => {
+    menuToggle();
+});
+$(window).on("resize", (e) => { 
+    if (e.target.innerHeight > 1060) {
+        menuToggle();
+    }
+});
