08975b7ec156143a1803e38dcd62e934c1bbee90
Aamir Zaidi <aamir.zaidi2000@gmail.com>
2020-05-27 21:18:13 +0530

    Responsive Work Done For Home And About Me Page

--- js/index.js
+++ js/index.js
@@ -7,22 +7,23 @@ const menu = $(".navItem_wrapper > ul");
 const redirectContact = () => {
     window.location.href = "./index.html#contactMe";
 
-}
+};
 const redirectAbout = () => {
     window.location.href = "./about.html";
-}
+};
 
 
 const menuToggle = () => {
     $(menu).toggleClass("show");
-}
+};
 
 
 $(mobile_btn).on("click", () => {
     menuToggle();
 });
-$(window).on("resize", (e) => { 
+$(window).on("resize", (e) => {
     if (e.target.innerHeight > 1060) {
         menuToggle();
     }
 });
+
